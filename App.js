5915cc8301dd56ac6d9d96ab5101d9a0d76dd367
ragnarr18 <ragnarr18@ru.is>
2020-12-07 20:12:12 +0000

    sdfsfdd

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Provider } from 'react-redux';
+import { Provider, connect } from 'react-redux';
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './src/reducers';
 import Navigator from './src/routes';
+import { getCinemas } from './src/actions/cinemasActions';
+import CinemaList from './src/components/cinemaList';
+
+// class SubApp extends React.Component{
+//   componentDidMount() {
+//     this.props.getCinemas();
+//     console.log(this.props);
+//   }
+//   render() {
+//     return(
+//       <CinemaList />
+//       // <Navigator />
+//     )
+//   }
+// }
+
+// const ConnectedSubApp = connect(null, { getCinemas })(SubApp)
 
 export default function App() {
   return (
     <Provider store={createStore(reducers, applyMiddleware(thunk))}>
+      {/* <ConnectedSubApp /> */}
       <Navigator />
     </Provider>
   );
--- src/components/cinemaList/index.js
+++ src/components/cinemaList/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
-// import CinemaListItem from '../cinemaDetailsListItem';
+import CinemaListItem from '../cinemaDetailsListItem';
 
 class cinemaList extends React.Component {
   constructor(props) {
