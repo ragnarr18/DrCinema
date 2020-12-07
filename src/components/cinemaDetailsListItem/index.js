59e258504ef63b1845b471ba96044b2f63623952
Arnar Snaer <arnarsnaer98@gmail.com>
2020-12-07 15:05:02 +0000

    Cinema details added

---
+++ src/components/cinemaDetailsListItem/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  View, Text,
+} from 'react-native';
+import styles from './styles';
+
+class CinemaDetailsListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>General Kenobi.</Text>
+      </View>
+    );
+  }
+}
+
+export default CinemaDetailsListItem;
