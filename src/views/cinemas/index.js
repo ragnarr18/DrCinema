b7fb65b479ed43867129488ac7f386e111313b70
Bjabbjo <42970264+Bjabbjo@users.noreply.github.com>
2020-12-07 14:17:09 +0000

    files created

---
+++ src/views/cinemas/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class cinemas extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Cinema Screen</Text>
+      </View>
+    )
+  }
+}
