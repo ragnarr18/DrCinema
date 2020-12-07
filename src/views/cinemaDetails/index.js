409e935f0bf46f1c4298b6e58ba3bc00dd2a665d
Arnar Snaer <arnarsnaer98@gmail.com>
2020-12-07 14:27:56 +0000

    Basic setup of Cinema Details

---
+++ src/views/cinemaDetails/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import styles from './styles';
+
+class cinemaDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const { navigation } = this.props;
+
+    return (
+      <View>
+        <Text>Cinema Name: [TBD]</Text>
+        <Text>Cinema Description: [TBD]</Text>
+        <Text>Cinema Address: [TBD]</Text>
+        <Text>Cinema Phone: [TBD]</Text>
+        <Text>Cinema Website: [TBD]</Text>
+
+        <Text>This is where I want a list to appear:</Text>
+      </View>
+    );
+  }
+}
+
+export default cinemaDetails;
