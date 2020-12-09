6d66b2ddcb859531f5e63530fd25372c7d70f1eb
Bjabbjo <42970264+Bjabbjo@users.noreply.github.com>
2020-12-09 16:57:21 +0000

    movie titles now show up under each cinema

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,6 +5,7 @@ import currentCinema from './currentCinemaReducer';
 import movies from './moviesReducer';
 import navigationRedux from './navigationReducer';
 import upcomingMovies from './upcomingMoviesReducer';
+import currentMovie from './currentMovieReducer';
 // import nav from './nav';
 
 export default combineReducers({
@@ -14,5 +15,6 @@ export default combineReducers({
   navigationRedux,
   movies,
   upcomingMovies,
+  currentMovie,
   // nav,
 });
