e88db039cd3a7942b4f6e34d9afb3412167e6b92
Arnar Snaer <arnarsnaer98@gmail.com>
2020-12-10 14:06:39 +0000

    Most movie detail items inserted

--- src/views/movieDetails/index.js
+++ src/views/movieDetails/index.js
@@ -11,54 +11,37 @@ import ShowtimeList from '../../components/showtimeList';
 class MovieDetails extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      name: 'Tom & Jerry',
-      releaseDate: '1920',
-      duration: '120 min',
-      image: 'https://upload.wikimedia.org/wikipedia/en/5/5f/TomandJerryTitleCardc.jpg',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque viverra nulla vel lorem convallis finibus. Sed commodo neque eget magna porta auctor. Quisque eget quam ultrices orci finibus...',
-      genres: 'action',
-    };
+    this.state = {};
   }
 
   render() {
-    const poster = <Image style={styles.image} source={{ uri: this.state.image }} resizeMode="cover" />;
+    const movie = this.props.currentMovie;
+    const poster = <Image style={styles.image} source={{ uri: movie.poster }} resizeMode="cover" />;
     console.log('currentMovie: ', this.props.currentMovie);
     // HERE IS THE MOVIE
     return (
       <View>
         <ScrollView contentContainer={{ flex: 1, flexGrow: 1 }}>
-          <View style={{ padding: 20, margin: 10 }}>
-            <Text>video</Text>
-            <Video
-              source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
-              rate={1.0}
-              volume={1.0}
-              isMuted={false}
-              resizeMode="cover"
-              shouldPlay
-              isLooping
-              style={{ width: 300, height: 300 }}
-            />
-          </View>
           <View style={styles.itemContainer}>
-            <Text style={styles.title}>{this.state.name}</Text>
+            <Text style={styles.title}>{movie.title}</Text>
             <Text style={styles.subtitle}>
               Released in:
               {' '}
-              {this.state.releaseDate}
+              {movie.year}
             </Text>
             <Text style={styles.subtitle}>
               Duration:
               {' '}
-              {this.state.duration}
+              {movie.durationMinutes}
+              {' '}
+              min
             </Text>
             {poster}
-            <Text>{this.state.description}</Text>
+            <Text>{movie.plot}</Text>
             <Text style={styles.genre}>
               Genres:
               {' '}
-              {this.state.genres}
+              TO-DO
             </Text>
           </View>
           <Text style={styles.title}>Sýningatímar</Text>
