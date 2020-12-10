e4006571f8b3265de815fdd6e80db286866e124b
Arnar Snaer <arnarsnaer98@gmail.com>
2020-12-10 14:00:10 +0000

    Basic setup of Movie Details

---
+++ src/components/showtimeList/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Dimensions } from 'react-native';
+
+const { width: winWidth } = Dimensions.get('window');
+const { height: winHeight } = Dimensions.get('window');
+
+export default StyleSheet.create({
+  list: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+  }
+});
